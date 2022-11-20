3c6d35c7772c80a77251da5371aae0a2e189e7b6
Jayasurya.K <jayasurya7aa@gmail.com>
2022-11-21 01:58:37 +0530

    fixed some errors

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -10,7 +10,7 @@ export const exerciseOptions = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-      'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+      'X-RapidAPI-Key': 'a055207c06mshc2e36897142c33bp1d51a6jsnf4a2edde9e71',
     },
   };
   
